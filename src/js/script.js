e1d0c4dcd2c53b247fe142d41f93d7a7209177cc
Your Name <tkorhonen13@gmail.com>
2021-07-11 23:02:33 +0300

    First commit

---
+++ src/js/script.js
@@ -0,0 +1,53 @@
+import "./../scss/main.scss"
+/*
+ * This app shows the weather of the city you input
+ *
+*/
+
+let cityName = document.getElementById("name");
+let weatherDescription = document.getElementById("desc");
+let tempIcon = document.getElementById("tempIcon")
+let temperature = document.getElementById("temp");
+
+/*  
+ * The getWeather function gets the current weather data.
+ * Uses openweathermap api
+ * Gives the info to the displayWeather function from the api
+*/ 
+function getWeather(city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="
+    + city 
+    +"&units=metric&appid=0516e1e3ca438425f653ec5340fe40d9")
+    .then( (response) => response.json())
+    .then((data) => displayWeather(data)); 
+    
+}
+/*  
+ * The displayWeather function gets the needed info from the api.
+ * Inserts the wanted data into dom
+*/ 
+function displayWeather(data) {
+    const { name } = data;
+    const { temp } = data.main;
+    const { icon, description } = data.weather[0];
+    
+    cityName.innerText = name;
+    temperature.innerText = Math.round(temp) + "°C";
+    weatherDescription.innerText = description;
+    tempIcon.src ="https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+    tempIcon.style.display = "inline-block";
+}
+/* 
+ * Eventlisteners to input and button(button is hidden in app for now)
+*/
+document.getElementById("submitButton")
+.addEventListener("click", () => {
+    getWeather(document.getElementById("inputValue").value)
+});
+document.getElementById("inputValue").addEventListener("keypress", (event)=> {
+    if (event.key == "Enter") {
+        document.getElementById("submitButton").click();
+    }
+    
+});
+
